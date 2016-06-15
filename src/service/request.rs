5f3a397ff280ff1f0da0790915d8c6e5d6d1d7e3
ppamorim <pp.amorim@hotmail.com>
2016-06-15 22:09:09 +0100

    Qt added

--- src/service/request.rs
+++ src/service/request.rs
@@ -1,7 +1,7 @@
 fn requestSomething() {
-  let client = Client::new();
-  let respose = client.get("www.google.com").send().unwrap();
-  if res.status == hyper::Ok {
-    println!("success")
-  }
+  // let client = Client::new();
+  // let respose = client.get("www.google.com").send().unwrap();
+  // if res.status == hyper::Ok {
+  //   println!("success")
+  // }
 }
